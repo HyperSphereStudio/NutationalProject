d2218b1b6a786dc452bc0245c84e62186cd302d5
John <johnathan.bizzano@gmail.com>
2024-04-21 08:51:55 -0400

    Version 2

--- System.jl
+++ System.jl
@@ -10,73 +10,111 @@ q_1 = qrotation(SVec(0, 1, 0), pi/2)
 
 """
 
+#=
+An Introduction to Physically Based Modeling:
+Rigid Body Simulation I—Unconstrained Rigid
+Body Dynamics
+
+(2) The Quaternions with an application to Rigid
+Body Dynamics
+Evangelos A. Coutsias† and Louis Romero‡
+Department of Mathematics and Statistics,
+University of New Mexico
+=#
+
 mutable struct SystemState
     #Constants
-    mass::Float64                       #Mass of the system
-    pivot_to_com_l::SVec                #Pivot point to center of Mass Fixed Frame
-    q_r_l::Quaternionf                  #q to reaction wheel frame
-    J_l::SVec                           #Diagonal Matrix Moment of Inertia of Body as vector in fixed frame
-    Jr_l::Svec                          #Diagonal Matrix Moment of Inertia of Reaction as vector in fixed frame
-
-    #Measured
-    direction_of_gravity_l::SVec        #Accelerometer.    Will be oscilliating along the pivot. Used to derive q
+    mass::Float32                       #Mass of the system
+    pivot_to_com_bf::SVec               #Pivot point to center of mass fixed frame
+    J_bf::SVec                          #Diagonal Matrix Moment of Inertia of Body as vector in body fixed frame
+    Jr_bf::SVec                         #Diagonal Matrix Moment of Inertia of Reaction as vector in body fixed frame
+    u_τ_bf::SVec                        #Unit direction of control torque in the body frame
 
     #Interpolated / Measured
-    q::Quaternionf                      #Intertial to Fixed Frame
-    ω::Float32                          #Gyroscope.        Should be relatively constant
 
-    #Calculated
-    ω_l::MVec                           #Angular Velocity Intertial Frame
-    h_l::MVec                           #Reaction Angular Momentum Intertial Frame
-    pivot_to_com_g::SVec                #Pivot point to center of Mass Intertial Frame
+    #x_sp = q * x_bf -> 8.1 The Euler equations for rigid body dynamics (2)
+    q::Quaternionf                      #Intertial to Fixed Frame.      Derived from accelerometer
+    ω_bf::Float32                       #Measuered by Gyroscope.        Should be relatively constant
+    h_bf::SVec                          #Angular momentum of reaction in body fixed frame. Can measure?  
+                            
+    h_dot_bf::MVec                      #
+    ω_dot_bf::MVec                      #Angular Acceleration in the Body Fixed Frame
 
-    τ_g::MVec                           #External Torques in the Intertial Frame
-    h_dot_g::MVec                       #Reaction Torque in the Intertial Frame
+    α::Float32                          #Desired Angle (cos(θ))
+
+    array::Array{Float32}               #Data array to integrate over
+
+    SystemState() = new()
 end
 
-function compute_and_expand_state_from_array(s::SystemState, v, t)
+function expand_state_from_array(s::SystemState)
+    v = s.array
     ptr = 1
-    rb.q, ptr = expand!(rb.q, v, ptr)
-    rb.ω_g, ptr = expand!(rb.ω_g, v, ptr)
-    rb.h_g, ptr = expand!(rb.h_g, v, ptr)
+    s.q, ptr = expand!(s.q, v, ptr)
+    s.ω_bf, ptr = expand!(s.ω_bf, v, ptr)
+    s.h_bf, ptr = expand!(s.h_bf, v, ptr)
+    s.h_dot_bf .= 0
+    s.ω_dot_bf .= 0
 end 
 
-function apply_state_to_array(rb::RotationalRigidBody, v)
+function apply_state_to_array(s::SystemState)
+    v = s.array
     ptr = 1
-    ptr = flatten!(rb.q, v, ptr)
-    ptr = flatten!(rb.ω_g, v, ptr)
-    ptr = flatten!(rb.h_g, v, ptr)
+    ptr = flatten!(s.q, v, ptr)
+    ptr = flatten!(s.ω_bf, v, ptr)
+    ptr = flatten!(s.h_bf, v, ptr)
 end
 
+#Eulers Rotational Equation
+#Differential Equation governing angular velocity in fixed frame
+
+"Jα + ω × (J ω) = T_ext + u"
+xdot(s) = (f(s) + β(s)) ./ s.J_bf        #J-1Jωdot = wdot
+
+
+#System Dynamics
+#Eulers Rotational Equation for a body-fixed 3 vector ω
+"ω × (J ω) = T_ext"
 function f(s::SystemState)
-    #Eulers Rotational Equation from
-    (rb.τ_g - cross(rb.ω_l, rb.J_l .* rb.ω_l)
+    τ_bf = SVec(0, 0, 0)
+    return τ_bf - cross(s.ω_bf, s.J_bf .* s.ω_bf)           
 end
 
-function compute_derivative_state(rb::RotationalRigidBody, vdot)
-    qdot = Quaternionf(rb.ω_g..., 0.0) * rb.q  / 2                  #q_dot = omega_g * q / 2
+#Reaction Rotational Dynamics
+#Eulers Rotational Equation for a body-fixed 3 vector ω
+β(s::SystemState) = -(s.h_dot_bf + cross(s.ω_bf, s.h_bf))
+
+#Control that forces the leypanov time derivative 0
+#J1α1 + ω × (J1 ω1) = -u1
+function u(s::SystemState)
+    c = [1, 1, 1]       #Coefficient Vector Weight the 
+
+    -(f(s) + β(s)) + c .* [s.ω_l[1], s.ω_l[2], -(1 - s.ω_l[3] / s.ω * α)]
+end
+
+"ḣ = (ĥ ⋅ u)ĥ = Proj_h(u)"
+reaction_control(s::SystemState) = s.h_dot_bf = dot(s.u_τ_bf, u(s)) * u_τ_bf
+
+function apply_derivative_state(s::SystemState)
+    xdot = s.array
+
+    #=dqdt = 1/2qΩ =#
+    qdot = s.q * Quaternionf(s.ω_l..., 0.0)  / 2                  #q_dot = q * omega_l / 2
 
     ptr = 1
-    ptr = flatten!(qdot, vdot, ptr)
-
-    #Jα + ω × (J ω) = T_ext + u1 + u2
-    ωdot_g = rb.J_i_l * ) 
-    ptr = flatten!(ωdot_g, vdot, ptr)
-    ptr = flatten!(rb.h_dot_g, vdot, ptr)
+    ptr = flatten!(qdot, xdot, ptr)
+    ptr = flatten!(s.ω_dot_bf, xdot, ptr)
+    ptr = flatten!(s.h_dot_bf, xdot, ptr)
 end
 
-#Rigid Body System
-function f(q, x, J)
+function state(s::SystemState)
+    expand_state_from_array(s)
 
+   # s.h_dot_l .= reaction_control(s)
+    s.ω_dot_bf .= xdot(s)
+
+    apply_derivative_state(s)
 end
-
-function state(x, h, hdot)
-
-
-    (x, h)
-end
-
-
 
 lerp(x, y, t) = t*x + (1-t)*y
 Base.eachindex(q::Quaternion) = eachindex(q.data)
